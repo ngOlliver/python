1f539eed78be273f70c3e51747946c14db159138
Natã Gabriel de Oliveira <nataneioliveira32@gmail.com>
2024-07-05 17:36:21 -0300

    Iniciando no python

---
+++ exercicios/ex1/hello,world!.py
@@ -0,0 +1,13 @@
+print('Hello, World!')
+print(7 + 4)
+print('7' + '4')
+#print('Olá' + 5) Não funciona concatenar strings e numbers com +
+print('Olá', 5)
+'''
+Mas funciona com vírgula
+'''
+
+nome = 'Guanabara'
+idade = 25
+peso = 75.8
+print(nome, idade, peso)
