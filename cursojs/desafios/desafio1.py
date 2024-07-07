330e9be3ab4de8db73be4c878128d2465996cb48
Natã Gabriel de Oliveira <nataneioliveira32@gmail.com>
2024-07-07 08:52:49 -0300

    Melhorando exercícios

--- cursojs/desafios/desafio1.py
+++ cursojs/desafios/desafio1.py
@@ -1,2 +1,2 @@
 nome = input('Qual é o seu nome?')
-print('Seja muito bem vindo, ' + nome + '!')
+print('Seja muito bem vindo, ', nome, '!', sep = '')
--- cursojs/desafios/desafio3.py
+++ cursojs/desafios/desafio3.py
@@ -1,3 +1,3 @@
-num1 = input('Primeiro número = ')
-num2 = input('Segundo número = ')
-print('A soma é' + (num1 + num2))
+num1 = int(input('Primeiro número = '))
+num2 = int(input('Segundo número = '))
+print('A soma é ', num1 + num2, sep = '')
