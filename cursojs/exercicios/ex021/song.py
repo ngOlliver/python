b4c4a3fb6a6fae4896337188e1490054ed23ee56
Natã Gabriel de Oliveira <nataneioliveira32@gmail.com>
2024-07-15 20:17:39 -0300

    Comentando erro

--- cursojs/exercicios/ex021/song.py
+++ cursojs/exercicios/ex021/song.py
@@ -1,5 +1,5 @@
-import pygame
-pygame.init()
-pygame.mixer.music.load('ex21.mp3')
-pygame.mixer.music.play()
-pygame.event.wait()
+# import pygame
+# pygame.init()
+# pygame.mixer.music.load('ex21.mp3')
+# pygame.mixer.music.play()
+# pygame.event.wait()
