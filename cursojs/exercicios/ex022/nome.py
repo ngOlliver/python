b5c79c0356f09120f205532ce23791cd4fbe5689
Natã Gabriel de Oliveira <nataneioliveira32@gmail.com>
2024-07-16 09:43:10 -0300

    Nova forma de resolver o exercício

--- cursojs/exercicios/ex022/nome.py
+++ cursojs/exercicios/ex022/nome.py
@@ -1,6 +1,13 @@
 nome = str(input('Digite seu nome completo: '))
-print(nome.upper())
-print(nome.lower())
-print(len(nome.replace(' ', '')))
+print('O seu nome com letras maiúsculas fica {}'.format(nome.upper()))
+print('O seu nome com letras minúsculas fica {}'.format(nome.lower()))
+print('O seu nome tem um total de {} letras'.format(len(nome.replace(' ', ''))))
 primeiro_nome = nome.split()
-print(len(primeiro_nome[0]))
+print('Seu primeiro nome tem {} letras'.format(len(primeiro_nome[0]))) # ou...
+
+
+nome = str(input('Digite seu nome completo: ')).strip()
+print('O seu nome com letras maiúsculas fica {}'.format(nome.upper()))
+print('O seu nome com letras minúsculas fica {}'.format(nome.lower()))
+print('O seu nome tem um total de {} letras'.format(len(nome) - nome.count(' ')))
+print('Seu primeiro nome tem {} letras'.format(nome.find(' '))) 
